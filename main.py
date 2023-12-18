82fe97cbb5fb2b2806fd960a31938b3dfab2507f
dhiyarisalah <18220047@std.stei.itb.ac.id>
2023-12-18 16:34:23 +0700

    change file structure

---
+++ main.py
@@ -0,0 +1,42 @@
+# MAIN
+import uvicorn
+from fastapi import FastAPI, Depends
+from fastapi.middleware.cors import CORSMiddleware
+from routers import auth, users, iteniary
+from dependencies import get_current_user
+from routers.iteniary import router as iteniary_router
+
+app = FastAPI(title="Destinology", description="Itinerary Planner API")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_credentials=True,
+    allow_origins=["*"],  # Adjust in production
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(
+    auth.router,
+    prefix="/auth",
+    tags=["Authentication"],
+    responses={404: {"description": "Not found"}}
+)
+app.include_router(
+    users.router,
+    prefix="/users",
+    tags=["Users"],
+    responses={404: {"description": "Not found"}},
+    dependencies=[Depends(get_current_user)],
+)
+
+app.include_router(
+    iteniary_router,
+    prefix="/models",
+    tags=["Itinerary Planner"],
+    responses={404: {"description": "Not found"}},
+    dependencies=[Depends(get_current_user)]
+)
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", reload=True)
---
+++ routers/users.py
@@ -0,0 +1,73 @@
+from fastapi import APIRouter, Request
+from fastapi import APIRouter, Depends, HTTPException, status
+
+from dependencies import get_current_user
+
+from schemas.users import UserSchema
+from schemas.auth import PasswordResetSchema
+from fastapi.responses import JSONResponse
+from utils import firestore_db, ExceptionResponse
+from firebase_admin import auth
+from utils import user_exists
+
+router = APIRouter()
+
+
+# Get User Profile
+@router.get("/me")
+async def get_user(current_user: auth.UserRecord = Depends(get_current_user)):
+    try:
+        user_doc_ref = firestore_db.collection("users").document(current_user.uid)
+        user_doc = await user_doc_ref.get()  # Use 'await' to get the document
+        return user_doc.to_dict() if user_doc.exists else {"message": "User not found"}
+    except Exception as e:
+        return ExceptionResponse(e)
+
+
+@router.put("/update")
+async def update_user(updated_user: UserSchema, current_user: auth.UserRecord = Depends(get_current_user)):
+    try:
+        # Update Firebase Auth User
+        auth.update_user(
+            current_user.uid,
+            email=updated_user.email,
+            display_name=updated_user.full_name
+        )
+        # Update Firestore User Document
+        user_doc_ref = firestore_db.collection("users").document(current_user.uid)
+        await user_doc_ref.set({
+            "email": updated_user.email,
+            "username": updated_user.username,
+            "full_name": updated_user.full_name
+        }, merge=True)
+        user_doc = await user_doc_ref.get()  # Use 'await' to get the document
+        return user_doc.to_dict()
+    except Exception as e:
+        return ExceptionResponse(e)
+
+
+@router.delete("/delete")
+async def delete_user(current_user: auth.UserRecord = Depends(get_current_user)):
+    try:
+        user_doc_ref = firestore_db.collection("users").document(current_user.uid)
+        
+        # Asynchronous deletion of the Firestore document
+        await user_doc_ref.delete()
+
+        # Delete the user from Firebase Auth
+        auth.delete_user(current_user.uid)
+
+        return {"message": "User deleted successfully"}
+    except Exception as e:
+        # Log the exception for debugging
+        print(f"Error in deleting user: {e}")
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+# @router.post("/me/password-reset")
+# async def reset_password(password_reset: PasswordResetSchema, current_user: auth.UserRecord = Depends(get_current_user)):
+#     try:
+#         auth.update_user(current_user.uid, password=password_reset.new_password)
+#         return {"message": "Password updated successfully"}
+#     except Exception as e:
+#         return ExceptionResponse(e)
